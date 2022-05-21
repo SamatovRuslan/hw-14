934f5218d2799cccd003831a5b7ad6974af655b1
SamatovRuslan <85801346+SamatovRuslan@users.noreply.github.com>
2022-05-21 18:28:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState} from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const ref = useRef(null);
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    ref.current.style.backgroundColor = 'green'
+    setTimeout(() => {
+      ref.current.style.backgroundColor = 'red'
+      setCount(count+1)
+    }, 3000);
+    
+  }, [count]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="square" ref={ref} >
     </div>
   );
 }
-
-export default App;
